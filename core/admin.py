b122023528d4ab33c98e43d326ce090aefe18c15
pivnenkowebdev <pivnenkowevdev@gmail.com>
2026-01-16 13:05:32 +0300

    feat: add templates and login

--- core/admin.py
+++ core/admin.py
@@ -4,4 +4,4 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'role', 'is_staff', 'is_active')
+    list_display = ('username', 'role')
