eb4d87bf064846cc3075315c58f8046f01100f22
pivnenkowebdev <pivnenkowevdev@gmail.com>
2026-01-21 10:38:55 +0300

    feat: registermodels

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,22 @@
-# Register your models here.
 from django.contrib import admin
-from .models import User
+from .models import (
+    User,
+    SchoolClass,
+    TeacherProfile,
+    Student,
+    Parent,
+    Message,
+    FAQ
+)
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'role')
+
+
+admin.site.register(SchoolClass)
+admin.site.register(TeacherProfile)
+admin.site.register(Student)
+admin.site.register(Parent)
+admin.site.register(Message)
+admin.site.register(FAQ)
--- core/views.py
+++ core/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.decorators import login_required
 
 from .models import TeacherProfile, Parent, Student, Message
 
-
 @login_required
 def dashboard(request):
     user = request.user
